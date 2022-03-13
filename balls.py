2ea7ccc532b5c840ba502a8db7898dd25fbb0605
CriticalHex <kubaska05@gmail.com>
2022-03-13 00:42:59 -0700

    Hehe, balls.

--- balls.py
+++ balls.py
@@ -1,35 +1,47 @@
 '''Libraries'''
-import math
+from math import sin, cos, pi
 import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800,800))
 pygame.display.set_caption("Bouncing Balls")
 screen.fill((0,0,0))
+clock = pygame.time.Clock()
+
+def draw_lines(ball1, ball2):
+    '''Connects two balls'''
+    pygame.draw.aaline(screen, (150,100,0), (ball1.pos_x, ball1.pos_y), (ball2.pos_x, ball2.pos_y))
+
 
 class Ball:
     '''Creates a ball'''
-    def __init__(self,x_pos,y_pos,):
-        self.pos_x = x_pos
-        self.pos_y = y_pos
+    def __init__(self,radius):
+        self.radius = radius
+        self.velocity = .0003
+        self.angle = 3*pi/2
+        self.pos_x = self.radius * (cos(self.angle)) + 400
+        self.pos_y = self.radius * (sin(self.angle)) + 700
     def __del__(self):
         pass
-    def update(self,x_off,y_off):
+    def move(self):
         '''Updates position of ball'''
-        self.pos_x += x_off
-        self.pos_y += y_off
+        self.angle += self.velocity
+        self.pos_x = self.radius * (cos(self.angle)) + 400
+        self.pos_y = self.radius * (sin(self.angle)) + 700
+        self.collide()
+        self.draw()
     def draw(self):
         '''Draws the ball'''
         pygame.draw.circle(screen, (200, 100, 0), (self.pos_x, self.pos_y), 10)
-    def collide(self, x, y):
+    def collide(self):
         '''Checks for collision'''
-        if math.sqrt(((self.pos_x-x) * (self.pos_x-x)) + ((self.pos_y-y) * (self.pos_y-y))) <= 10:
-            return True
-        return False
-
-BALLS = [Ball((90+(i*15))+12, (90+(i*15))+290) for i in range(16)]
+        if self.pos_y - 700 >= (-abs(self.pos_x - 400)) - 10:
+            self.velocity *= -1
+            print(self.pos_x, self.pos_y)
+BALLS = [Ball((i*25) + 100) for i in range(16)]
 EXIT = False
 while not EXIT:
+    #clock.tick(6000)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             EXIT = True
@@ -37,7 +49,10 @@ while not EXIT:
     screen.fill((0,0,0))
     pygame.draw.line(screen, (255,255,255), (400,700), (50,350), 2)
     pygame.draw.line(screen, (255,255,255), (400,700), (750,350), 2)
+    for i in range(len(BALLS)):
+        if i < len(BALLS) - 1:
+            draw_lines(BALLS[i],BALLS[i+1])
     for balls in BALLS:
-        balls.draw()
+        balls.move()
     pygame.display.flip()
 pygame.quit()
