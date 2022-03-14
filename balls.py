c43e099a615ed465e015f6cd6e45dbd48e6079a8
CriticalHex <kubaska05@gmail.com>
2022-03-13 23:56:44 -0600

    Several updates

--- balls.py
+++ balls.py
@@ -1,12 +1,15 @@
 '''Libraries'''
-from math import sin, cos, pi
+from math import sin, cos, sqrt
 import pygame
 pygame.init()
 
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((1000,1000))
 pygame.display.set_caption("Bouncing Balls")
 screen.fill((0,0,0))
 clock = pygame.time.Clock()
+
+ORIGIN_X = 500
+ORIGIN_Y = 900
 
 def draw_lines(ball1, ball2):
     '''Connects two balls'''
@@ -15,44 +18,47 @@ def draw_lines(ball1, ball2):
 
 class Ball:
     '''Creates a ball'''
-    def __init__(self,radius):
+    def __init__(self,radius,velocity):
         self.radius = radius
-        self.velocity = .0003
-        self.angle = 3*pi/2
-        self.pos_x = self.radius * (cos(self.angle)) + 400
-        self.pos_y = self.radius * (sin(self.angle)) + 700
+        self.velocity = velocity
+        self.angle = 3.76
+        self.pos_x = self.radius * (cos(self.angle)) + ORIGIN_X
+        self.pos_y = (1.5 * self.radius) * (sin(self.angle)) + ORIGIN_Y
     def __del__(self):
         pass
     def move(self):
         '''Updates position of ball'''
         self.angle += self.velocity
-        self.pos_x = self.radius * (cos(self.angle)) + 400
-        self.pos_y = self.radius * (sin(self.angle)) + 700
-        self.collide()
-        self.draw()
+        self.pos_x = self.radius * (cos(self.angle)) + ORIGIN_X
+        self.pos_y = (2 * self.radius) * (sin(self.angle)) + ORIGIN_Y
     def draw(self):
         '''Draws the ball'''
         pygame.draw.circle(screen, (200, 100, 0), (self.pos_x, self.pos_y), 10)
     def collide(self):
         '''Checks for collision'''
-        if self.pos_y - 700 >= (-abs(self.pos_x - 400)) - 10:
+        if self.pos_y - ORIGIN_Y >= (-abs(self.pos_x - ORIGIN_X)) - 10:
             self.velocity *= -1
-            print(self.pos_x, self.pos_y)
-BALLS = [Ball((i*25) + 100) for i in range(16)]
+BALLS = [Ball((i*10) + 200, (.029) * (5/8)) for i in range(16)]
 EXIT = False
+TICKER = 0
 while not EXIT:
-    #clock.tick(6000)
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             EXIT = True
     #render----------------------------------------------------------------
     screen.fill((0,0,0))
-    pygame.draw.line(screen, (255,255,255), (400,700), (50,350), 2)
-    pygame.draw.line(screen, (255,255,255), (400,700), (750,350), 2)
-    for i in range(len(BALLS)):
-        if i < len(BALLS) - 1:
+    pygame.draw.line(screen, (255,255,255), (ORIGIN_X,ORIGIN_Y), (ORIGIN_X - 400,ORIGIN_Y - 400), 2)
+    pygame.draw.line(screen, (255,255,255), (ORIGIN_X,ORIGIN_Y), (ORIGIN_X + 400,ORIGIN_Y- 400), 2)
+    # pygame.draw.line(screen, (255,255,255), (ORIGIN_X-400,ORIGIN_Y), (ORIGIN_X+400,ORIGIN_Y), 1)
+    # pygame.draw.line(screen, (255,255,255), (ORIGIN_X,ORIGIN_Y-700), (ORIGIN_X,800), 1)
+    for i in range(16):
+        if i < 15:
             draw_lines(BALLS[i],BALLS[i+1])
     for balls in BALLS:
+        balls.draw()
+        balls.collide()
         balls.move()
     pygame.display.flip()
+    TICKER += 1
 pygame.quit()
